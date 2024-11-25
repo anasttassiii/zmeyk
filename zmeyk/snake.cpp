149e21480ffcb39ceb96c53f90569033602c571a
Анастасия Метелкина <nm.met@mail.ru>
2024-11-25 18:18:47 +0700

    Добавление статического поля и метода, дружественной функции и std::vector

--- zmeyk/snake.cpp
+++ zmeyk/snake.cpp
@@ -3,217 +3,224 @@
 #include <ctime>
 #include <windows.h>
 #include <conio.h>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
 using namespace std;
+int Snake::nTail = 0;
+
+int getTailLength(const Snake& s) {
+    return Snake::nTail;
+}
+
 void Game::Draw_horizontal_borders()
 {
-	for (int i = 0; i < width + 1;i++) {
-		cout << "#";
-	}
-	cout << endl;
+    for (int i = 0; i < width + 1;i++) {
+        cout << "#";
+    }
+    cout << endl;
 }
 
 bool Game::Draw_vertical_borders(int y_coordinate, int x_coordinate)
 {
-	if (x_coordinate == 0 || x_coordinate == width - 1) {
-		cout << '#';
-		return true;
-	}
-	return false;
+    if (x_coordinate == 0 || x_coordinate == width - 1) {
+        cout << '#';
+        return true;
+    }
+    return false;
 }
 
 bool Game::Draw_head_of_snake(int y_coordinate, int x_coordinate)
 {
-	if (y_coordinate == yHead && x_coordinate == xHead)
-	{
-		cout << "0";
-		return true;
-	}
-	return false;
+    if (y_coordinate == yHead && x_coordinate == xHead)
+    {
+        cout << "0";
+        return true;
+    }
+    return false;
 }
 
 bool Game::Draw_Fruite(int y_coordinate, int x_coordinate)
 {
-	if (y_coordinate == fruitY && x_coordinate == fruitX)
-	{
-		cout << "F";
-		return true;
-	}
-	return false;
+    if (y_coordinate == fruitY && x_coordinate == fruitX)
+    {
+        cout << "F";
+        return true;
+    }
+    return false;
 }
 
 void Game::Draw_Snake_tail_or_space(int y_coordinate, int x_coordinate)
 {
-	bool print = false;
-	for (int k = 0; k < nTail; k++)
-		if (tailX[k] == x_coordinate && tailY[k] == y_coordinate)
-		{
-			print = true;
-			cout << "o";
-		}
-	if (!print)
-		cout << ' ';
+    bool print = false;
+    int& nTailRef = Snake::getnTailRef(); // Получаем ссылку
+    for (int k = 0; k < nTailRef; k++)
+        if (tailX[k] == x_coordinate && tailY[k] == y_coordinate)
+        {
+            print = true;
+            cout << "o";
+        }
+    if (!print)
+        cout << ' ';
 }
 
-void Game::Draw()
-{
-	system("cls");
-	Draw_horizontal_borders();
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			if (Draw_vertical_borders(i, j));
-			if (Draw_head_of_snake(i, j));
-			else
-			{
-				if (Draw_Fruite(i, j));
-				else {
-					Draw_Snake_tail_or_space(i, j);
-				}
-			}
-		}
-		cout << endl;
-	}
-	Draw_horizontal_borders();
-	cout << "Score: " << score << endl;
+void Game::Draw() {
+    system("cls");
+    Draw_horizontal_borders();
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (Draw_vertical_borders(i, j));
+            if (Draw_head_of_snake(i, j));
+            else {
+                if (Draw_Fruite(i, j));
+                else {
+                    Draw_Snake_tail_or_space(i, j);
+                }
+            }
+        }
+        cout << endl;
+    }
+    Draw_horizontal_borders();
+    cout << "Score: " << score << endl;
 }
 
 void Game::Input()
 {
-	if (_kbhit())
-	{
-		switch (_getch())
-		{
-		case 'a':
-		{
-			dir = LEFT;
-			break;
-		}
-		case 'd':
-		{
-			dir = RIGHT;
-			break;
-		}
-		case 'w':
-		{
-			dir = UP;
-			break;
-		}
-		case 's':
-		{
-			dir = DOWN;
-			break;
-		}
-		case 'x':
-		{
-			gameover = true;
-			break;
-		}
-		}
-	}
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+        {
+            dir = LEFT;
+            break;
+        }
+        case 'd':
+        {
+            dir = RIGHT;
+            break;
+        }
+        case 'w':
+        {
+            dir = UP;
+            break;
+        }
+        case 's':
+        {
+            dir = DOWN;
+            break;
+        }
+        case 'x':
+        {
+            gameover = true;
+            break;
+        }
+        }
+    }
 }
 
-void Game::Tail_step()
-{
-	int prevX = tailX[0];
-	int prevY = tailY[0];
-	int prev2X, prev2Y;
-	tailX[0] = xHead;
-	tailY[0] = yHead;
-	for (int i = 1; i < nTail; i++)
-	{
-		prev2X = tailX[i];
-		prev2Y = tailY[i];
-		tailX[i] = prevX;
-		tailY[i] = prevY;
-		prevX = prev2X;
-		prevY = prev2Y;
-	}
+void Game::Tail_step() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = xHead;
+    tailY[0] = yHead;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
 }
 
 void Game::Change_of_head_position()
 {
-	switch (dir)
-	{
-	case LEFT:
-	{
-		xHead--;
-		break;
-	}
-	case RIGHT:
-	{
-		xHead++;
-		break;
-	}
-	case UP:
-	{
-		yHead--;
-		break;
-	}
-	case DOWN:
-	{
-		yHead++;
-		break;
-	}
-	}
+    switch (dir)
+    {
+    case LEFT:
+    {
+        xHead--;
+        break;
+    }
+    case RIGHT:
+    {
+        xHead++;
+        break;
+    }
+    case UP:
+    {
+        yHead--;
+        break;
+    }
+    case DOWN:
+    {
+        yHead++;
+        break;
+    }
+    }
 }
 
 void Game::meeting_with_boorder()
 {
-	if (xHead >= width - 1)
-		xHead = 0;
-	else if (xHead < 0)
-		xHead = width - 2;
-	if (yHead >= height)
-		yHead = 0;
-	else if (yHead < 0)
-		yHead = height - 1;
+    if (xHead >= width - 1)
+        xHead = 0;
+    else if (xHead < 0)
+        xHead = width - 2;
+    if (yHead >= height)
+        yHead = 0;
+    else if (yHead < 0)
+        yHead = height - 1;
 }
 
 void Game::head_to_tail_check()
 {
-	for (int i = 0; i < nTail; i++)
-	{
-		if (tailX[i] == xHead && tailY[i] == yHead)
-			gameover = true;
-	}
+    int* nTailPtr = Snake::getnTailPtr(); // Получаем указатель
+    for (int i = 0; i < *nTailPtr; i++)
+    {
+        if (tailX[i] == xHead && tailY[i] == yHead)
+            gameover = true;
+    }
 }
 
 bool Game::Check_Tail_and_Fruit_coincidence()
 {
-	bool f = true;
-	for (int i = 0; i < nTail && f; i++)
-	{
-		f = false;
-		if (tailX[i] == fruitX && tailY[i] == fruitY)
-		{
-			fruitX = rand() % (width - 1);
-			fruitY = rand() % height;
-			f = true;
-		}
-		else f = false;
-	}
-	return f;
+    bool f = true;
+    int* nTailPtr = Snake::getnTailPtr(); // Получаем указатель
+    for (int i = 0; i < *nTailPtr && f; i++)
+    {
+        f = false;
+        if (tailX[i] == fruitX && tailY[i] == fruitY)
+        {
+            fruitX = rand() % (width - 1);
+            fruitY = rand() % height;
+            f = true;
+        }
+        else f = false;
+    }
+    return f;
 }
 
 void Game::Eating_Fruits()
 {
-	if (xHead == fruitX && yHead == fruitY)
-	{
-		score += 10;
-		fruitX = rand() % (width - 1);
-		fruitY = rand() % height;
-		if (nTail > 0)
-			while (Check_Tail_and_Fruit_coincidence());
-		nTail++;
-	}
+    if (xHead == fruitX && yHead == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % (width - 1);
+        fruitY = rand() % height;
+        if (nTail > 0)
+            while (Check_Tail_and_Fruit_coincidence());
+        plusnTail();
+    }
 }
 
 void Game::Logic()
 {
-	Tail_step();
-	Change_of_head_position();
-	meeting_with_boorder();
-	head_to_tail_check();
-	Eating_Fruits();
+    Tail_step();
+    Change_of_head_position();
+    meeting_with_boorder();
+    head_to_tail_check();
+    Eating_Fruits();
 }
--- zmeyk/snake.h
+++ zmeyk/snake.h
@@ -1,55 +1,63 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include <ctime>
+#include <string>
 
-enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 
 class Snake {
 protected:
-	int width;
-	int height;
-	int tailX[100], tailY[100];   // в массивах хранятся координаты хвоста змейки
-	int nTail;      // длина хвоста
+    int width;
+    int height;
+    std::vector<int> tailX;  // Замена массива на вектор
+    std::vector<int> tailY;  // Замена массива на вектор
+    static int nTail; // длина хвоста
+
 public:
-	Snake()        // конструктор, задающий начальные значения змейки
-	{
-		width = 20;
-		height = 15;
-		nTail = 0;
-	}
+    Snake() { // конструктор, задающий начальные значения змейки
+        this->width = 20;
+        this->height = 15;
+    }
+    friend int getTailLength(const Snake& s); // Дружественная функция
+    static int plusnTail() { return ++nTail; }
+
+    static int* getnTailPtr() { return &nTail; }
+
+    static int& getnTailRef() { return nTail; }
 };
 
-class Game : public Snake
-{
+class Game : public Snake {
 public:
-	eDirection dir;
-	bool gameover;
-	int xHead, yHead, fruitX, fruitY, score;  // координаты головы змейки, положения фрукта
-	Game() :Snake()   // конструктор, задающий начальное положение игры
-	{
-		srand(time(NULL));
-		gameover = false;
-		dir = STOP;
-		xHead = (width / 2) - 1;
-		yHead = (height / 2) - 1;
-		fruitX = rand() % (width - 1);
-		fruitY = rand() % height;
-		score = 0;
-	}
-	void Draw();   // рисуем поле
-	void Input();   // получаем входные данные
-	void Logic();  // реализуем логику игры
-	void Draw_horizontal_borders();   // рисуем горизонтальные стенки
-	bool Draw_vertical_borders(int, int); // рисуем вертикальные стенки
-	bool Draw_head_of_snake(int, int); // рисуем голову змейки
-	bool Draw_Fruite(int, int); // рисуем фрукт
-	void Draw_Snake_tail_or_space(int, int);
-	void Tail_step();
-	void Change_of_head_position();
-	void Eating_Fruits();
-	void head_to_tail_check();
-	void meeting_with_boorder();
-	bool Check_Tail_and_Fruit_coincidence();
+    eDirection dir;
+    bool gameover;
+    int xHead, yHead, fruitX, fruitY, score; // координаты головы змейки, положения фрукта
+
+    Game() : Snake() { // конструктор, задающий начальное положение игры
+        srand(time(NULL));
+        gameover = false;
+        dir = STOP;
+        xHead = (this->width / 2) - 1; // Использование this для доступа к члену класса
+        yHead = (this->height / 2) - 1;
+        fruitX = rand() % (this->width - 1);
+        fruitY = rand() % this->height;
+        score = 0;
+        tailX.resize(100); // Резервируем место для 100 элементов
+        tailY.resize(100); // Резервируем место для 100 элементов
+    }
+
+    void Draw(); // рисуем поле
+    void Input(); // получаем входные данные
+    void Logic(); // реализуем логику игры
+    void Draw_horizontal_borders(); // рисуем горизонтальные стенки
+    bool Draw_vertical_borders(int, int); // рисуем вертикальные стенки
+    bool Draw_head_of_snake(int, int); // рисуем голову змейки
+    bool Draw_Fruite(int, int); // рисуем фрукт
+    void Draw_Snake_tail_or_space(int, int);
+    void Tail_step();
+    void Change_of_head_position();
+    void Eating_Fruits();
+    void head_to_tail_check();
+    void meeting_with_boorder();
+    bool Check_Tail_and_Fruit_coincidence();
 };
-
-
